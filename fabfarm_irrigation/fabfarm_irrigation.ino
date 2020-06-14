edcc21dd268ddb05a0be5b5d8f7c199237bbed28
Lucio <lucio@fabfarm.org>
2020-06-14 15:58:32 +0100

    new version compiling with external file system

--- fabfarm_irrigation/fabfarm_irrigation.ino
+++ fabfarm_irrigation/fabfarm_irrigation.ino
@@ -8,6 +8,12 @@
  *  A Portion of this code was based on Shakeels code for ESP8266 ;
  *  My contributions: 
  *     -So far I made it work on platformio :), that took me quite a lot of time
+ *     -That means:
+ *        +created a new project;
+ *        +created a folder named data under the main folder (fabfarm_irrigation)
+ *        +linked the platformio.ini to the folder of the project + the data folder
+ *        +linked the needed libraries to their github repo in the platformio.ini
+ *        +found a conflict with time library and A
  *  Things I still want to program for my final project:
  *    -so far I ported Shakeels code into ESP32;
  *    -separate functions from shakeels code into files;
@@ -25,57 +31,86 @@
  ****************************************************************************/
  
 
-
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
-// Import required libraries
+//Required Libraries
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
+#include <AsyncTCP.h>
 
-// Replace with your network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+// Network Credentials
+const char* ssid = "rato";
+const char* password = "imakestuff";
 
-// Set LED GPIO
-const int ledPin = 2;
-// Stores LED state
-String ledState;
-
-// Create AsyncWebServer object on port 80
+//Start the Async Web Server listening on port 80
 AsyncWebServer server(80);
 
-// Replaces placeholder with LED state value
+// Set to true to define Relay as Normally Open (NO)
+#define RELAY_NO false
+
+// Set number of relays, will be used in the array
+#define NUM_RELAYS  4
+
+// Assign each GPIO to a relay
+int relayGPIOs[NUM_RELAYS] = {26, 25, 33, 32};
+
+//
+const char* PARAM_INPUT_1 = "relay";  
+const char* PARAM_INPUT_2 = "state";
+
+// Replaces placeholder with button section in your web page
 String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(ledPin)){
-      ledState = "ON";
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    for(int i=1; i<=NUM_RELAYS; i++){
+      String relayStateValue = relayState(i);
+      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
     }
-    else{
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
+    return buttons;
   }
   return String();
 }
- 
+
+String relayState(int numRelay){
+  if(RELAY_NO){
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "";
+    }
+    else {
+      return "checked";
+    }
+  }
+  else {
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "checked";
+    }
+    else {
+      return "";
+    }
+  }
+  return "";
+}
+
 void setup(){
   // Serial port for debugging purposes
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-
+  Serial.begin(9600);
   // Initialize SPIFFS
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-
-  // Connect to Wi-Fi
+  // Set all relays to off when the program starts - if set to Normally Open (NO), the relay is off when you set the relay to HIGH
+  for(int i=1; i<=NUM_RELAYS; i++){
+    pinMode(relayGPIOs[i-1], OUTPUT);
+    if(RELAY_NO){
+      digitalWrite(relayGPIOs[i-1], HIGH);
+    }
+    else{
+      digitalWrite(relayGPIOs[i-1], LOW);
+    }
+  }
+  
+  // Connect the ESP to the Wi-Fi using the credentials entered before
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -85,32 +120,57 @@ void setup(){
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
 
+/*
+*Now we are going to configure the route where server will be listening for incoming HTTP requests 
+and a function that will be executed when a request is received on that route.
+We specify this by calling the "on" method on the server object. With server.on(){}; 
+As first input, this method receives a string with the path where it will be listening. 
+We are going to set it to listen for requests on the “/” route. This could be anything. 
+It is basically what you write after the ip adress when in the browser or an APP.
+So... 
+- First parameter here is: "/" thats the root directory.
+- Second parameter is HTTP_GET thats an enum of type WebRequestMethod a method defined in the library here --> https://github.com/me-no-dev/ESPAsyncWebServer/blob/63b5303880023f17e1bca517ac593d8a33955e94/src/ESPAsyncWebServer.h
+- Third parameter is a the function AsyncWebServerRequest
+*/
+
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
 
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, HIGH);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+ // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    String inputMessage2;
+    String inputParam2;
+    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      inputParam2 = PARAM_INPUT_2;
+      if(RELAY_NO){
+        Serial.print("NO ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
+      }
+      else{
+        Serial.print("NC ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage + inputMessage2);
+    request->send_P(200, "text/plain", "OK");
   });
 
   // Start server
   server.begin();
 }
- 
+
 void loop(){
-  
+
 }
