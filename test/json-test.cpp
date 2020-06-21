4ceaffcf42d84c8ee0c79b63d927556a43889af6
Jeffrey Knight <jknight@derivadex.com>
2020-06-21 16:33:02 +0100

    Proof of concept for json

--- test/json-test.cpp
+++ test/json-test.cpp
@@ -16,39 +16,46 @@ Sample json for https://arduinojson.org/v6/assistant/
 {
     "data": { "time": 1351824120, "temperature": 23, "humidity": 45}, 
     "zones": [
-      { "name": "valve1", "pin": 123,  "status": 1 , "duration": 1000, "times": ["00:00", "01:00"]}, 
-      { "name": "valve2", "pin": 234, "status": 1 , "duration": 3000, "times": ["00:00", "01:00"]}, 
-      { "name": "valve3", "pin": 345, "status": 0, "duration": 2000, "times": ["00:00", "01:00"]}
+      { "name": "relay1", "pin": 123,  "status": 1 , "duration": 1000, "times": ["00:00", "01:00"]}, 
+      { "name": "relay2", "pin": 234, "status": 1 , "duration": 3000, "times": ["00:00", "01:00"]}, 
+      { "name": "relay3", "pin": 345, "status": 0, "duration": 2000, "times": ["00:00", "01:00"]}
     ]
   }
 */
 
 using namespace std;
 
+// global cache of current json
+DynamicJsonDocument doc(2048);
+
 // prototypes
 void serialize();
 void test(const char*);
 void deserialize(const char*);
+void loopOnce();
 
 int main() { 
 
     // Write json to a file ...
+    /*
     const char *output = "test.json";
     ofstream f;
     f.open (output);
-    const char* json = "{\"data\":{\"time\":1351824120,\"temperature\":23,\"humidity\":45},\"valves\":[{\"name\":\"valve1\",\"pin\":123,\"status\":1,\"duration\":1000,\"times\":[\"00:00\",\"01:00\"]},{\"name\":\"valve2\",\"pin\":234,\"status\":1,\"duration\":3000,\"times\":[\"00:00\",\"01:00\"]},{\"name\":\"valve3\",\"pin\":345,\"status\":10,\"duration\":2000,\"times\":[\"00:00\",\"01:00\"]}]}";
+    const char* json = "{\"data\":{\"time\":1351824120,\"temperature\":23,\"humidity\":45},\"relays\":[{\"name\":\"relay1\",\"pin\":123,\"status\":1,\"duration\":1000,\"times\":[\"00:00\",\"01:00\"]},{\"name\":\"relay2\",\"pin\":234,\"status\":1,\"duration\":3000,\"times\":[\"00:00\",\"01:00\"]},{\"name\":\"relay3\",\"pin\":345,\"status\":10,\"duration\":2000,\"times\":[\"00:00\",\"01:00\"]}]}";
     f <<  json;
     f.close();
+    */
 
     // Read json from the file ...
     ostringstream sstream;
-    ifstream fs("test.json");
+    ifstream fs("hello.txt");
     sstream << fs.rdbuf();
     const string str(sstream.str());
     const char* ptr = str.c_str();
+    cout<<"Got json:\n"<<ptr<<"\n";
 
     // deserialize json and update some vales 
-    test(ptr);
+    //test(ptr);
 
     return 0;
 }
@@ -56,10 +63,11 @@ int main() {
 // Confirm that we can take json, deserialize it, set new values, and reserialize it back to Json (yes)
 void test(const char* json) {
 
-    const size_t capacity = 3*JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(5) + 210;
-    DynamicJsonDocument doc(capacity);
-    // start with json string 
-    deserializeJson(doc, json);
+    //Does this need to change whenever we alter our json ? :(. Can we just do a 1024 or some large enough value on it? (YES?)
+    //const size_t capacity = 3*JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(5) + 210; 
+    //DynamicJsonDocument doc(2048);
+    //json string to object
+    deserializeJson(doc, json); 
 
     //we'll want to actually *set* values here then serialize back to json 
     JsonObject data = doc["data"];
@@ -74,51 +82,64 @@ void test(const char* json) {
     serializeJson(doc, output);
     //deserializeJson(doc, Serial);
     cout<<"Got updated json:\n"<<output<<"\n";
+
+    loopOnce();
+}
+
+void loopOnce() {
+
+    //JsonObject data = doc["data"];
+    //JsonArray relays = doc.createNestedArray("relays");
+    JsonArray relays = doc["relays"];
+    cout<<"Relays ...\n"<<relays.size()<<"\n";
+    for(int i = 0; i < relays.size(); i++) {
+        cout<<"-->"<<i<<"\n";
+    }
 }
 
 void serialize() {
 
     //TODO: do we need to change this every time we change our json ? :(
-    const size_t capacity = 3*JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(5);
+    //const size_t capacity = 3*JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(5);
+    //DynamicJsonDocument doc(capacity);
 
-    DynamicJsonDocument doc(capacity);
-
-    JsonObject data = doc.createNestedObject("data");
+    JsonObject data = doc["data"];
+    data = doc.createNestedObject("data");
     data["time"] = 1351824120;
     data["temperature"] = 23;
     data["humidity"] = 45;
 
-    JsonArray valves = doc.createNestedArray("valves");
+    JsonArray relays = doc.createNestedArray("relays");
 
-    JsonObject valves_0 = valves.createNestedObject();
-    valves_0["name"] = "valve1";
-    valves_0["pin"] = 123;
-    valves_0["status"] = 1;
-    valves_0["duration"] = 1000;
+    JsonObject relays_0 = relays.createNestedObject();
+    relays_0["name"] = "relay1";
+    relays_0["pin"] = 123;
+    relays_0["status"] = 1;
+    relays_0["duration"] = 1000;
 
-    JsonArray valves_0_times = valves_0.createNestedArray("times");
-    valves_0_times.add("00:00");
-    valves_0_times.add("01:00");
+    JsonArray relays_0_times = relays_0.createNestedArray("times");
+    relays_0_times.add("00:00");
+    relays_0_times.add("01:00");
 
-    JsonObject valves_1 = valves.createNestedObject();
-    valves_1["name"] = "valve2";
-    valves_1["pin"] = 234;
-    valves_1["status"] = 1;
-    valves_1["duration"] = 3000;
+    JsonObject relays_1 = relays.createNestedObject();
+    relays_1["name"] = "relay2";
+    relays_1["pin"] = 234;
+    relays_1["status"] = 1;
+    relays_1["duration"] = 3000;
 
-    JsonArray valves_1_times = valves_1.createNestedArray("times");
-    valves_1_times.add("00:00");
-    valves_1_times.add("01:00");
+    JsonArray relays_1_times = relays_1.createNestedArray("times");
+    relays_1_times.add("00:00");
+    relays_1_times.add("01:00");
 
-    JsonObject valves_2 = valves.createNestedObject();
-    valves_2["name"] = "valve3";
-    valves_2["pin"] = 345;
-    valves_2["status"] = 10;
-    valves_2["duration"] = 2000;
+    JsonObject relays_2 = relays.createNestedObject();
+    relays_2["name"] = "relay3";
+    relays_2["pin"] = 345;
+    relays_2["status"] = 10;
+    relays_2["duration"] = 2000;
 
-    JsonArray valves_2_times = valves_2.createNestedArray("times");
-    valves_2_times.add("00:00");
-    valves_2_times.add("01:00");
+    JsonArray relays_2_times = relays_2.createNestedArray("times");
+    relays_2_times.add("00:00");
+    relays_2_times.add("01:00");
 
     //File file = SD.open("out.json", FILE_WRITE);
     string output;
@@ -141,32 +162,32 @@ void deserialize(const char* json) {
     int data_temperature = data["temperature"]; // 23
     int data_humidity = data["humidity"]; // 45
 
-    JsonArray valves = doc["valves"];
+    JsonArray relays = doc["relays"];
 
-    JsonObject valves_0 = valves[0];
-    const char* valves_0_name = valves_0["name"]; // "valve1"
-    int valves_0_pin = valves_0["pin"]; // 123
-    int valves_0_status = valves_0["status"]; // 1
-    int valves_0_duration = valves_0["duration"]; // 1000
+    JsonObject relays_0 = relays[0];
+    const char* relays_0_name = relays_0["name"]; // "relay1"
+    int relays_0_pin = relays_0["pin"]; // 123
+    int relays_0_status = relays_0["status"]; // 1
+    int relays_0_duration = relays_0["duration"]; // 1000
 
-    const char* valves_0_times_0 = valves_0["times"][0]; // "00:00"
-    const char* valves_0_times_1 = valves_0["times"][1]; // "01:00"
+    const char* relays_0_times_0 = relays_0["times"][0]; // "00:00"
+    const char* relays_0_times_1 = relays_0["times"][1]; // "01:00"
 
-    JsonObject valves_1 = valves[1];
-    const char* valves_1_name = valves_1["name"]; // "valve2"
-    int valves_1_pin = valves_1["pin"]; // 234
-    int valves_1_status = valves_1["status"]; // 1
-    int valves_1_duration = valves_1["duration"]; // 3000
+    JsonObject relays_1 = relays[1];
+    const char* relays_1_name = relays_1["name"]; // "relay2"
+    int relays_1_pin = relays_1["pin"]; // 234
+    int relays_1_status = relays_1["status"]; // 1
+    int relays_1_duration = relays_1["duration"]; // 3000
 
-    const char* valves_1_times_0 = valves_1["times"][0]; // "00:00"
-    const char* valves_1_times_1 = valves_1["times"][1]; // "01:00"
+    const char* relays_1_times_0 = relays_1["times"][0]; // "00:00"
+    const char* relays_1_times_1 = relays_1["times"][1]; // "01:00"
 
-    JsonObject valves_2 = valves[2];
-    const char* valves_2_name = valves_2["name"]; // "valve3"
-    int valves_2_pin = valves_2["pin"]; // 345
-    int valves_2_status = valves_2["status"]; // 10
-    int valves_2_duration = valves_2["duration"]; // 2000
+    JsonObject relays_2 = relays[2];
+    const char* relays_2_name = relays_2["name"]; // "relay3"
+    int relays_2_pin = relays_2["pin"]; // 345
+    int relays_2_status = relays_2["status"]; // 10
+    int relays_2_duration = relays_2["duration"]; // 2000
 
-    const char* valves_2_times_0 = valves_2["times"][0]; // "00:00"
-    const char* valves_2_times_1 = valves_2["times"][1]; // "01:00"
+    const char* relays_2_times_0 = relays_2["times"][0]; // "00:00"
+    const char* relays_2_times_1 = relays_2["times"][1]; // "01:00"
 }
