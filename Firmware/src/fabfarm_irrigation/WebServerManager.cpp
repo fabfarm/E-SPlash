17343f996cb366ae015749eee5ef73c9354bacbe
lpgn <lpgn@hotmail.com>
2023-06-15 16:52:20 +0100

    refactor

--- Firmware/src/fabfarm_irrigation/WebServerManager.cpp
+++ Firmware/src/fabfarm_irrigation/WebServerManager.cpp
@@ -1,39 +1,22 @@
 #include "WebServerManager.h"
 #include "Config.h"
-void serverHandle() {
-    server.on("/data.json", HTTP_GET, handleDataJsonRequest);
-    AsyncCallbackJsonWebHandler *updateData = new AsyncCallbackJsonWebHandler("/updateData", handleUpdateDataRequest);
-    server.on("^\\/mode\\/((manual)|(scheduled))$", HTTP_GET, handleModeRequest);
-    server.on("^\\/relay\\/([0-9]+)\\/((on)|(off))$", HTTP_GET, handleRelayRequest);
-    AsyncCallbackJsonWebHandler *updateRelayTime = new AsyncCallbackJsonWebHandler("/relay/update-time", handleUpdateRelayTimeRequest);
-    AsyncCallbackJsonWebHandler *addRelayTime = new AsyncCallbackJsonWebHandler("/relay/add-time", handleAddRelayTimeRequest);
-    server.on("^\\/relay\\/([0-9]+)\\/time\\/([0-9]+)$", HTTP_DELETE, handleDeleteRelayTimeRequest);
-    AsyncCallbackJsonWebHandler *addRelay = new AsyncCallbackJsonWebHandler("/relay/add", handleAddRelayRequest);
-    server.on("^\\/relay\\/([0-9]+)$", HTTP_DELETE, handleDeleteRelayRequest);
 
-    server.addHandler(updateData);
-    server.addHandler(updateRelayTime);
-    server.addHandler(addRelayTime);
-    server.addHandler(addRelay);
+// HTTP status codes
+const int HTTP_OK = 200;
+const int HTTP_INTERNAL_SERVER_ERROR = 500;
+const int HTTP_METHOD_NOT_ALLOWED = 405;
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(SPIFFS, "/index.html", String(), false);
-    });
-
-    server.on("^(\\/[a-zA-Z0-9_.-]*)$", HTTP_GET, [](AsyncWebServerRequest *request) {
-        String file = request->pathArg(0);
-        Serial.printf("Serving file %s\n\r", file.c_str());
-        request->send(SPIFFS, file, String(), false);
-    });
-
-    isScheduleMode = doc["data"]["isScheduleMode"];
-    if (!isScheduleMode) {
-        manualMode();
-    }
+// Helper function to serialize JSON and send response
+void sendJsonResponse(AsyncWebServerRequest *request, DynamicJsonDocument &data)
+{
+    char jsonReply[1800];
+    serializeJson(data, jsonReply);
+    request->send(HTTP_OK, "application/json", jsonReply);
 }
 
-
-void handleDataJsonRequest(AsyncWebServerRequest *request) {
+// Handler for GET /data.json
+void handleGetDataJsonRequest(AsyncWebServerRequest *request)
+{
     Serial.println("/data.json");
     DynamicJsonDocument data = doc;
 
@@ -42,44 +25,55 @@ void handleDataJsonRequest(AsyncWebServerRequest *request) {
     data["data"]["humidity"] = readDHTHumidity();
     data["data"]["batLevel"] = getBatteryLevel();
 
-    char jsonReply[1800];
-    Serial.println("Serialize JSON & return to caller - BEGIN");
-    serializeJson(data, jsonReply);
-    Serial.println(jsonReply);
-    request->send(200, "application/json", jsonReply);
-    Serial.println("Serialize JSON & return to caller - COMPLETE");
+    sendJsonResponse(request, data);
 }
 
-void handleUpdateDataRequest(AsyncWebServerRequest *request, JsonVariant &json) {
+// Handler for POST /updateData
+void handleUpdateDataRequest(AsyncWebServerRequest *request, JsonVariant &json)
+{
     doc = json.as<JsonObject>();
 
-    if (writeDataJson()) {
-        request->send(200);
-    } else {
-        request->send(500);
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleModeRequest(AsyncWebServerRequest *request) {
+// Handler for GET /mode/(manual|scheduled)
+void handleModeChangeRequest(AsyncWebServerRequest *request)
+{
     isScheduleMode = request->pathArg(0).equals("scheduled");
     doc["data"]["isScheduleMode"].set(isScheduleMode);
 
-    if (writeDataJson()) {
-        if (isScheduleMode) {
+    if (writeDataJson())
+    {
+        if (isScheduleMode)
+        {
             scheduleMode();
-        } else {
+        }
+        else
+        {
             manualMode();
         }
 
-        request->send(200);
-    } else {
-        request->send(500);
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleRelayRequest(AsyncWebServerRequest *request) {
-    if (isScheduleMode) {
-        request->send(405);
+// Handler for GET /relay/(number)/(on|off)
+void handleRelayRequest(AsyncWebServerRequest *request)
+{
+    if (isScheduleMode)
+    {
+        request->send(HTTP_METHOD_NOT_ALLOWED);
     }
 
     int relayIndex = request->pathArg(0).toInt();
@@ -88,53 +82,73 @@ void handleRelayRequest(AsyncWebServerRequest *request) {
     Serial.printf("Relay %d: %d\n\r", relayIndex, state);
     doc["relays"][relayIndex]["isEnabled"].set(state);
 
-    if (writeDataJson()) {
+    if (writeDataJson())
+    {
         manualMode();
-        request->send(200);
-    } else {
-        request->send(500);
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleUpdateRelayTimeRequest(AsyncWebServerRequest *request, JsonVariant &json) {
+// Handler for POST /relay/update-time
+void handleUpdateRelayTimeRequest(AsyncWebServerRequest *request, JsonVariant &json)
+{
     int relayIndex = json["relayIndex"];
     int timeIndex = json["timeIndex"];
     doc["relays"][relayIndex]["times"][timeIndex]["startTime"].set(json["startTime"]);
     doc["relays"][relayIndex]["times"][timeIndex]["duration"].set(json["duration"]);
 
-    if (writeDataJson()) {
-        request->send(200);
-    } else {
-        request->send(500);
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleAddRelayTimeRequest(AsyncWebServerRequest *request, JsonVariant &json) {
+// Handler for POST /relay/add-time
+void handleAddRelayTimeRequest(AsyncWebServerRequest *request, JsonVariant &json)
+{
     JsonObject nested = doc["relays"][json["relayIndex"]]["times"].as<JsonArray>().createNestedObject();
     nested["startTime"] = "10:00";
     nested["duration"] = 30;
 
-    if (writeDataJson()) {
-        request->send(200);
-    } else {
-        request->send(500);
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleDeleteRelayTimeRequest(AsyncWebServerRequest *request) {
+// Handler for DELETE /relay/(number)/time/(number)
+void handleDeleteRelayTimeRequest(AsyncWebServerRequest *request)
+{
     int relayIndex = request->pathArg(0).toInt();
     int timeIndex = request->pathArg(1).toInt();
 
     doc["relays"][relayIndex]["times"].as<JsonArray>().remove(timeIndex);
 
-    if(writeDataJson()) {
-        request->send(200); 
-    } else {
-        request->send(500); 
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleAddRelayRequest(AsyncWebServerRequest *request, JsonVariant &json) {
+// Handler for POST /relay/add
+void handleAddRelayRequest(AsyncWebServerRequest *request, JsonVariant &json)
+{
     JsonObject nested = doc["relays"].as<JsonArray>().createNestedObject();
 
     const size_t CAPACITY = JSON_ARRAY_SIZE(3);
@@ -151,22 +165,62 @@ void handleAddRelayRequest(AsyncWebServerRequest *request, JsonVariant &json) {
     nested["pin"] = json["pin"];
     nested["isEnabled"] = false;
 
-    if (writeDataJson()) {
-        request->send(200);
-    } else {
-        request->send(500);
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
-void handleDeleteRelayRequest(AsyncWebServerRequest *request) {
+// Handler for DELETE /relay/(number)
+void handleDeleteRelayRequest(AsyncWebServerRequest *request)
+{
     int relayIndex = request->pathArg(0).toInt();
     Serial.printf("Removing relay %d\n\r", relayIndex);
     doc["relays"].as<JsonArray>().remove(relayIndex);
 
-    if (writeDataJson()) {
-        request->send(200);
-    } else {
-        request->send(500);
+    if (writeDataJson())
+    {
+        request->send(HTTP_OK);
+    }
+    else
+    {
+        request->send(HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
+void serverHandle()
+{
+    server.on("/data.json", HTTP_GET, handleGetDataJsonRequest);
+    AsyncCallbackJsonWebHandler *updateData = new AsyncCallbackJsonWebHandler("/updateData", handleUpdateDataRequest);
+    server.on("^\\/mode\\/((manual)|(scheduled))$", HTTP_GET, handleModeChangeRequest);
+    server.on("^\\/relay\\/([0-9]+)\\/((on)|(off))$", HTTP_GET, handleRelayRequest);
+    AsyncCallbackJsonWebHandler *updateRelayTime = new AsyncCallbackJsonWebHandler("/relay/update-time", handleUpdateRelayTimeRequest);
+    AsyncCallbackJsonWebHandler *addRelayTime = new AsyncCallbackJsonWebHandler("/relay/add-time", handleAddRelayTimeRequest);
+    server.on("^\\/relay\\/([0-9]+)\\/time\\/([0-9]+)$", HTTP_DELETE, handleDeleteRelayTimeRequest);
+    AsyncCallbackJsonWebHandler *addRelay = new AsyncCallbackJsonWebHandler("/relay/add", handleAddRelayRequest);
+    server.on("^\\/relay\\/([0-9]+)$", HTTP_DELETE, handleDeleteRelayRequest);
+
+    server.addHandler(updateData);
+    server.addHandler(updateRelayTime);
+    server.addHandler(addRelayTime);
+    server.addHandler(addRelay);
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/index.html", String(), false); });
+
+    server.on("^(\\/[a-zA-Z0-9_.-]*)$", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+        String file = request->pathArg(0);
+        Serial.printf("Serving file %s\n\r", file.c_str());
+        request->send(SPIFFS, file, String(), false); });
+
+    isScheduleMode = doc["data"]["isScheduleMode"];
+    if (!isScheduleMode)
+    {
+        manualMode();
+    }
+}
