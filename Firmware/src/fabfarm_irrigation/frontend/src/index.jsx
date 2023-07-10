c13b56c5620d371311b25d0b742a65842033061c
emastra <emiliano.mastra@gmail.com>
2023-07-10 15:50:18 +0100

    minor

--- Firmware/src/fabfarm_irrigation/frontend/src/index.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 import './styles/normalize.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
