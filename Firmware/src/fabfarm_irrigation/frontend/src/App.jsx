5f17d6aaabed9992d52a1342708b244c88c854f5
emastra <emiliano.mastra@gmail.com>
2023-07-10 12:12:29 +0100

    prettier config and formatting

--- Firmware/src/fabfarm_irrigation/frontend/src/App.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h2>Dashboard</h2>
-    </>
-  )
+    return (
+        <>
+            <h2>Dashboard</h2>
+        </>
+    );
 }
 
-export default App
+export default App;
