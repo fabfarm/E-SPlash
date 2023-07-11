0b8bc710641d46951308d26874add751c6a12853
emastra <emiliano.mastra@gmail.com>
2023-07-11 14:32:34 +0100

    basic controls style and html

--- Firmware/src/fabfarm_irrigation/frontend/src/components/Controls/Controls.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/Controls/Controls.jsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from 'react';
+import './Controls.css';
+
 const Controls = () => {
-    return <h2>Controls</h2>;
+    return (
+        <section className='controls'>
+            <header className='page-title'>
+                <h2>Controls</h2>
+            </header>
+
+            <div className='controls-container'>
+                <div className='control-box'>
+                    <div className='box-label'>Fruit (pin 27)</div>
+                    <div className='checkbox-wrapper'>
+                        <input type='checkbox' />
+                    </div>
+                </div>
+
+                <div className='control-box'>
+                    <div className='box-label'>Vegetables (pin 26)</div>
+                    <div className='checkbox-wrapper'>
+                        <input type='checkbox' />
+                    </div>
+                </div>
+
+                <div className='control-box'>
+                    <div className='box-label'>Water (pin 25)</div>
+                    <div className='checkbox-wrapper'>
+                        <input type='checkbox' />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Controls;
