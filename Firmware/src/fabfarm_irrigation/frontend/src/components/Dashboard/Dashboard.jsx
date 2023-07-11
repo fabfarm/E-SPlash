d85556fdd6e95e0a0d0327c39caa1c7f2614220e
emastra <emiliano.mastra@gmail.com>
2023-07-11 11:32:57 +0100

    dashboard and card styles

--- Firmware/src/fabfarm_irrigation/frontend/src/components/Dashboard/Dashboard.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/Dashboard/Dashboard.jsx
@@ -7,35 +7,31 @@ const Dashboard = () => {
                 <h2>Dashboard</h2>
             </header>
 
-            <div className='cards-wrapper'>
+            <div className='cards-container'>
                 <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
+                    <div>Monday, July 10 2023</div>
+                    <div className='card-value'>10:24</div>
                 </div>
 
                 <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
+                    <div className='icon-container'>
+                        <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
+                    </div>
+                    <div className='card-value'>32 C</div>
                 </div>
 
                 <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
+                    <div className='icon-container'>
+                        <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
+                    </div>
+                    <div className='card-value'>32 C</div>
                 </div>
 
                 <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
-                </div>
-
-                <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
-                </div>
-
-                <div className='card'>
-                    <div>icon</div>
-                    <div>value</div>
+                    <div className='icon-container'>
+                        <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
+                    </div>
+                    <div className='card-value'>32 C</div>
                 </div>
             </div>
         </section>
