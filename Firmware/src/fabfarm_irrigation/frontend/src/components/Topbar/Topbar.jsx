e1fe4296dc38737a005dafd3d9afb116604fe3f0
emastra <emiliano.mastra@gmail.com>
2023-07-11 14:59:56 +0100

    topbar styling

--- Firmware/src/fabfarm_irrigation/frontend/src/components/Topbar/Topbar.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/Topbar/Topbar.jsx
@@ -4,11 +4,12 @@ const Topbar = () => {
     return (
         <header className='topbar'>
             <div className='logo-wrapper'>
-                {/* <img src='/src/assets/fabfarm_logo_cut.png' alt='logo' /> */}
-                LOGO
+                <img className='logo' src='/src/assets/fabfarm_logo_cut.png' alt='logo' />
             </div>
-            <div>FABFARM Algarve</div>
-            <div>hamburger</div>
+            <div class='topbar-title'>FABFARM Algarve</div>
+            <div className='hamburger-container'>
+                <img className='hamburger' src='/src/assets/icons/hamburger.svg' alt='menu' />
+            </div>
         </header>
     );
 };
