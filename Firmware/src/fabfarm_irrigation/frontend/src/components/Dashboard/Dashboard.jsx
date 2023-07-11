9246f367c965aa0476a7f7e4db2a36fb02077204
emastra <emiliano.mastra@gmail.com>
2023-07-11 12:12:31 +0100

    mock data; test fetch data;

--- Firmware/src/fabfarm_irrigation/frontend/src/components/Dashboard/Dashboard.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const [data, setData] = useState({});
+
+    const fetchData = (url) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('### data:', data);
+                setData(data.data);
+            });
+    };
+
+    useEffect(() => {
+        fetchData('/src/mockData/data.json');
+    }, []);
+
     return (
         <section className='dashboard'>
             <header className='page-title'>
@@ -10,28 +26,28 @@ const Dashboard = () => {
             <div className='cards-container'>
                 <div className='card'>
                     <div>Monday, July 10 2023</div>
-                    <div className='card-value'>10:24</div>
+                    <div className='card-value'>{data.currentTime}</div>
                 </div>
 
                 <div className='card'>
                     <div className='icon-container'>
                         <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
                     </div>
-                    <div className='card-value'>32 C</div>
+                    <div className='card-value'>{data.temperature} C</div>
                 </div>
 
                 <div className='card'>
                     <div className='icon-container'>
                         <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
                     </div>
-                    <div className='card-value'>32 C</div>
+                    <div className='card-value'>{data.humidity}</div>
                 </div>
 
                 <div className='card'>
                     <div className='icon-container'>
                         <img src='/src/assets/icons/thermometer.svg' alt='thermometer' />
                     </div>
-                    <div className='card-value'>32 C</div>
+                    <div className='card-value'>{data.batLevel}</div>
                 </div>
             </div>
         </section>
