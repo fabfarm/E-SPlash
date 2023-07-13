42b8e366d14fc3c5247388ac50fe80ea58faeaf8
emastra <emiliano.mastra@gmail.com>
2023-07-13 11:42:42 +0100

    topbar router links

--- Firmware/src/fabfarm_irrigation/frontend/src/components/Topbar/Topbar.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/Topbar/Topbar.jsx
@@ -1,16 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Topbar.css';
 
 const Topbar = () => {
     return (
         <header className='topbar'>
-            <div className='logo-wrapper'>
+            <Link to='/' className='logo-wrapper'>
                 <img className='logo' src='/src/assets/fabfarm_logo_cut.png' alt='logo' />
                 <div className='logo-title'>FabFarm Algarve</div>
-            </div>
+            </Link>
 
             {/* <div className='app-title'>Irrigation system</div> */}
 
             <div className='hamburger-container'>
+                <Link to='/settings'>Settings</Link>
                 <img className='hamburger' src='/src/assets/icons/hamburger.svg' alt='menu' />
             </div>
         </header>
