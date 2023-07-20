2ce61d9a2edac6ad9eb420f65655249be217b98f
emastra <emiliano.mastra@gmail.com>
2023-07-20 18:17:39 +0100

    FirmwareUpdate; basic styles;

--- Firmware/src/fabfarm_irrigation/frontend/src/components/FirmwareUpdate/FirmwareUpdate.jsx
+++ Firmware/src/fabfarm_irrigation/frontend/src/components/FirmwareUpdate/FirmwareUpdate.jsx
@@ -1,5 +1,15 @@
 const FirmwareUpdate = () => {
-    return <div>FirmwareUpdate</div>;
+    return (
+        <section className='settings-box'>
+            <header>
+                <h2>Firmare Update</h2>
+            </header>
+
+            <div>
+                <button>Update firmare</button>
+            </div>
+        </section>
+    );
 };
 
 export default FirmwareUpdate;
